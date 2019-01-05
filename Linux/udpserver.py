1e94429b77437149cfd4659a285128f2aa7cd128
Olgierd <opilarczyk@gmail.com>
2019-01-05 23:54:36 +0100

    serial port update

--- Linux/udpserver.py
+++ Linux/udpserver.py
@@ -2,8 +2,11 @@
 
 import socket, serial
 
+serial_port = '/dev/ttyUSB0'
+
+
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-#ser = serial.Serial(port='/dev/ttyUSB0', baudrate=115200)
+ser = serial.Serial(port=serial_port, baudrate=115200)
 
 s.bind(('127.0.0.1', 31337))
 
@@ -12,6 +15,6 @@ lastmsg = ''
 while True:
 	data, addr = s.recvfrom(8)
 	if data != lastmsg:
-#		ser.write(data)
+		ser.write(data)
 		lastmsg = data
 		print data
