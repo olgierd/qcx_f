b598058488dba683aa35be7527c572cb50a228ec
Olgierd Pilarczyk <opilarczyk@egnyte.com>
2019-01-08 10:39:25 +0100

    comments cleanup

--- QCX_pyaudio/process.py
+++ QCX_pyaudio/process.py
@@ -24,32 +24,32 @@ cnt = 0         # symbols count
 zerocount = 0   # for counting zeros
 
 while True:
-    data = stream.read(chunk)   # read data and convert to signed 16b int
+    data = stream.read(chunk)                            # read data and convert to signed 16b int
     buf=[ numpy.fromstring(data[q:q+2], 'Int16') for q in range(0,len(data), 2)]
-    for i in range(0,len(buf)-1):   # for all samples
-        if buf[i] == 0 and zerocount < 100: # count 100 samples with '0'
+    for i in range(0,len(buf)-1):                        # for all samples
+        if buf[i] == 0 and zerocount < 100:              # count 100 samples with '0'
             zerocount = zerocount + 1
-            if zerocount == 100:    # if 100 zero samples in a row... (2.2ms @ 44.1kHz)
-                print "Z"           # stop TX
-                ser.write("Z")      # send "Z" via serial (= stop TX)
-                cnt = 0             # reset symbol counter
-        if buf[i] < 0 and buf[i+1] >= 0:    # zero crossing
-            zerocount = 0                   # clean 0-sample counter
+            if zerocount == 100:                         # if 100 zero samples in a row... (2.2ms @ 44.1kHz)
+                print "Z"                                # stop TX
+                ser.write("Z")                           # send "Z" via serial (= stop TX)
+                cnt = 0                                  # reset symbol counter
+        if buf[i] < 0 and buf[i+1] >= 0:                 # zero crossing
+            zerocount = 0                                # clean 0-sample counter
             crossing = i + float(-buf[i])/(-buf[i]+buf[i+1])    # calculate 0-crossing with sub-sample resolution
-            if lastc > 0:   # if there already was a crossing...
-                if lastc > crossing:    # crossing in the previous buffer?
-                    lastc = lastc-1024  # subtract buffer length
-                freq = RATE/(crossing-lastc)    # calculate sine frequency
+            if lastc > 0:                                # if there already was a crossing...
+                if lastc > crossing:                     # crossing in the previous buffer?
+                    lastc = lastc-1024                   # subtract buffer length
+                freq = RATE/(crossing-lastc)             # calculate sine frequency
                 if abs(freq-lastfreq) > 1 and abs(freq-lastfreq) < 50: # if it changed?
-                    delay = 5   # wait for 5 more crossings
-                if delay == 1:  # and print new current frequency..
-                    print cnt, "---", (lastfreq+freq)/2 # .. as average from last 2 crossings
+                    delay = 5                            # wait for 5 more crossings
+                if delay == 1:                           # and print new current frequency..
+                    print cnt, "---", (lastfreq+freq)/2  # .. as average from last 2 crossings
                     ser.write("A" + int( 10*(lastfreq+freq)/2) + "X")
-                    cnt = cnt+1     # increment symbol counter
-                if delay > 0:   # waiting
+                    cnt = cnt+1                          # increment symbol counter
+                if delay > 0:                            # waiting
                     delay = delay -1
-                lastfreq = freq    # memorize last frequency
-            lastc = crossing    # ..and position of last zero-crossing
+                lastfreq = freq                          # memorize last frequency
+            lastc = crossing                             # ..and position of last zero-crossing
 
 stream.stop_stream()
 stream.close()
