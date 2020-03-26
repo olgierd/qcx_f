7cdb728332e4edd5af798198334fe57cec05a71e
olgierd <opilarczyk@gmail.com>
2020-03-26 09:08:16 +0100

    Update for new WSJTX - support gradual freqeuncy shifts

--- QCX_pyaudio/process.py
+++ QCX_pyaudio/process.py
@@ -1,27 +1,27 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 # -*- coding: utf-8 -*-
 # de SQ3SWF 2019
 
 import pyaudio, numpy, serial
 
-# 1024 samples at a time, signed 16-bit int, 1 channel, 44100 Hz
-chunk = 1024
+# 512 samples at a time, signed 16-bit int, 1 channel, 44100 Hz
+chunk = 512
 FORMAT = pyaudio.paInt16
 CHANNELS = 1
 RATE = 44100
 SERIAL_BAUDRATE = 115200
 
 p = pyaudio.PyAudio() # init audio object
-ser = serial.Serial('/dev/ttyS0', 115200) # open serial port
+ser = serial.Serial('/dev/ttyUSB0', 115200) # open serial port
 
 # open audio stream
 stream = p.open(format=FORMAT, channels=CHANNELS, rate=RATE, input=True, output=True, frames_per_buffer=chunk)
 
 lastc = -1      # last crossing position
 lastfreq = 0      # lasf frequency
-delay = 5       # initial delay (in crossings) before first measure
 cnt = 0         # symbols count
 zerocount = 0   # for counting zeros
+currentf = 0
 
 while True:
     data = stream.read(chunk)                            # read data and convert to signed 16b int
@@ -30,26 +30,28 @@ while True:
         if buf[i] == 0 and zerocount < 100:              # count 100 samples with '0'
             zerocount = zerocount + 1
             if zerocount == 100:                         # if 100 zero samples in a row... (2.2ms @ 44.1kHz)
-                print "Z"                                # stop TX
-                ser.write("Z")                           # send "Z" via serial (= stop TX)
+                print("Z")                               # stop TX
+                ser.write(bytes("Z", "UTF-8"))           # send "Z" via serial (= stop TX)
                 cnt = 0                                  # reset symbol counter
+    lastc = 0
+    freqs = []
+    for i in range(0,len(buf)-1):                        # for all samples
         if buf[i] < 0 and buf[i+1] >= 0:                 # zero crossing
             zerocount = 0                                # clean 0-sample counter
-            crossing = i + float(-buf[i])/(-buf[i]+buf[i+1])    # calculate 0-crossing with sub-sample resolution
+            crossing = float(i + (-buf[i])/(-buf[i]+buf[i+1]))    # calculate 0-crossing with sub-sample resolution
             if lastc > 0:                                # if there already was a crossing...
-                if lastc > crossing:                     # crossing in the previous buffer?
-                    lastc = lastc-1024                   # subtract buffer length
                 freq = RATE/(crossing-lastc)             # calculate sine frequency
-                if abs(freq-lastfreq) > 1 and abs(freq-lastfreq) < 50: # if it changed?
-                    delay = 5                            # wait for 5 more crossings
-                if delay == 1:                           # and print new current frequency..
-                    print cnt, "---", (lastfreq+freq)/2  # .. as average from last 2 crossings
-                    ser.write("A" + str(int( 10*(lastfreq+freq)/2)) + "X")
-                    cnt = cnt+1                          # increment symbol counter
-                if delay > 0:                            # waiting
-                    delay = delay -1
-                lastfreq = freq                          # memorize last frequency
+                freqs.append(freq)
             lastc = crossing                             # ..and position of last zero-crossing
+        if len(freqs) > 5 or (i == len(buf)-2 and len(freqs) != 0):
+            freq = sum(freqs)/len(freqs)
+            if abs(freq - lastfreq) > 1:
+                print(cnt, freq)
+                lastfreq = freq
+                ser.write(bytes("A" + str(int( 100*(lastfreq+freq)/2)) + "X", "UTF-8"))
+                print("A" + str(int( 10*(lastfreq+freq)/2)) + "X")
+            cnt = cnt+1                          # increment symbol counter
+            break
 
 stream.stop_stream()
 stream.close()
